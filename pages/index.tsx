eaa878b88d394126fe87d4a92eb6d9568728391f
Valentin <valentin@gmail.com>
2023-05-30 23:54:57 -0300

    YSYA

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,22 +46,19 @@ const Home: NextPage = () => {
           <div className="p-8 md:p-12 lg:px-16 lg:py-24">
             <div className="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
               <h2 className="text-2xl font-bold text-white md:text-3xl">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit
+              ForLoop: Nuestra Historia
               </h2>
               <p className="hidden text-gray-500 md:mt-4 md:block">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas
-                tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et
-                fermentum, augue. Aliquet amet volutpat quisque ut interdum tincidunt
-                duis.
+              ForLoop, empresa desarrolladora de software con base en Argentina, se destaca por su enfoque innovador y calidad en soluciones a medida. Impulsamos el éxito de nuestros clientes con soluciones tecnológicas vanguardistas.
               </p>
               <div className="mt-4 md:mt-8">
                 <a href="/" className="bg-blue-600 rounded-xl inline-block px-12 py-3 text-sm font-medium text-white transition hover:bg-blue-500 focus:outline-none focus:ring focus:ring-yellow-400">
-                  Get Started Today
+                  Ver mas de Nosotros
                 </a>
               </div>
             </div>
           </div>
-          <img src="https://images.unsplash.com/photo-1484959014842-cd1d967a39cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="violin" className="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-ss-[30px] md:h-[calc(100%_-_4rem)] md:rounded-ss-[60px]" />
+          <img src="/nosotros.png" alt="violin" className="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-ss-[30px] md:h-[calc(100%_-_4rem)] md:rounded-ss-[60px]" />
         </section>
       </main>
       <Testimonials />
